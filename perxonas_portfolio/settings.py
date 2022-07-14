5befedfbe814725afb92971d3f3af6e21f242512
ansalim <ansalim14@gmail.com>
2022-07-14 22:19:59 +0000

    Remove old stuff

--- perxonas_portfolio/settings.py
+++ perxonas_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^8+76i0-f6!2u%ghc*eq&1%ld8b77kfw=-z0m^8e^m*+ygdhy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ansalim01.pythonanywhere.com']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'portfolio',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT= BASE_DIR / 'media'
